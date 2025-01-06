9a87f0c8ac9c7ffad392bc12a86e6413eafca4c8
sandredroll <sandhyakumari08315@gmail.com>
2025-01-06 19:49:37 +0530

    pushing the code to repo

---
+++ Day1/variables.js
@@ -0,0 +1,31 @@
+let str1="double quotes";
+console.log(str1,typeof(str1));
+
+let str2='single quotet';
+console.log(str2, typeof(str2));
+
+let str3='template literals';
+console.log(str3, typeof(str3));
+
+
+let bool1=true;
+console.log(bool1, typeof(bool1));
+
+let bool2=false;
+console.log(bool2, typeof(bool2));
+
+let num1=10;
+console.log(num1,typeof(num1));
+let num2=10.23456;
+console.log(num2, typeof(num2));
+let num3=10e3;
+console.log(num3,typeof(num3));
+
+let a;
+console.log(a, typeof(a));
+
+let x=null;
+console.log(x, typeof(x));
+
+
+
