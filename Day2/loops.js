658958373da21a468715af2cc3de3d7fc032493a
sandredroll <sandhyakumari08315@gmail.com>
2025-01-08 13:19:18 +0530

    new files added

---
+++ Day2/loops.js
@@ -0,0 +1,19 @@
+/*for(let i=1; i<=100; i++){
+    if(i%2==0){
+        console.log(i + " is Even number");
+
+    }
+    else{
+        console.log(i + " is Odd number")
+
+    }
+    console.log(" ");
+} */
+let mul=1
+let a=Number(prompt("enter the factorial value:"))
+    for(let i=1; i<=a; i++){
+        mul *= i
+}
+    console.log("return:"+mul
+
+    );
